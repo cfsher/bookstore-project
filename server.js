14976abebaa556fb8c1153bcbb7d9593e0597055
Chris Fisher <chrisfisher@Chriss-MBP.lan>
2020-04-14 20:18:47 -0400

    create user route updated

--- server.js
+++ server.js
@@ -3,6 +3,48 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 
+async function mailer(firstname, lastname, email) {
+  let account = await nodemailer.createTestAccount();
+  let transporter = nodemailer.createTransport(
+        {
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+              user: account.user,
+              pass: account.pass
+          },
+          logger: false,
+          debug: false // include SMTP traffic in the logs
+        },
+        {
+          // default message fields
+
+          // sender info
+          from: 'Bookstore <no-reply@bookstore.net>',
+          headers: {
+          }
+        }
+    );
+    let message = {
+         // Comma separated list of recipients
+         to: `${firstname} ${lastname} <${email}>`,
+
+         // Subject of the message
+         subject: 'Bookstore Registration Confirmation',
+
+         // plaintext body
+         text: `Thank you ${firstname} for registering for the bookstore!`,
+
+         // An array of attachments
+         attachments: []
+     };
+    let info = await transporter.sendMail(message);
+
+    console.log('Message sent successfully!');
+    console.log(nodemailer.getTestMessageUrl(info));
+}
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -12,13 +54,20 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// POST request to handle user account creation
 app.post('/user', (req, res) => {
-  
-  // TODO pass in data from request
-  // sql query with data
-  // send confirmation email
-  // send back if registration sucessful
+  user = req.body.user;
+  const sql = `INSERT INTO users (firstname, lastname, email, password, phone, address)
+          VALUES (${user.firstname}, ${user.lastname}, ${user.email}, ${user.password},
+          ${user.phone}, ${user.address})`;
+  connection.query(sql, (error, result) => {
+    if (error) throw err;
+    console.log('1 record inserted');
+    res.send('user added');
+    mailer(firstname, lastname, email).catch(err => {
+      console.error(err.message);
+      process.exit(1);
+    });
+  });
 });
 
 // POST request to handle login authentication
