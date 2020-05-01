e160b0d8590bb64876ccef4676c449acdcf2c65f
Chris Fisher <chrisfisher@Chriss-MBP.lan>
2020-04-30 20:13:38 -0400

    added server for testing frontend calls

---
+++ test-server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.post('/user', (req, res) => {
+  user = req.body.user;
+  console.log(user);
+});
+
+// POST request to handle login authentication
+app.post('/login', (req, res) => {
+  login = req.body.login;
+  console.log(login);
+});
+
+// POST request to handle edit profile actions
+app.post('editProfile', (req, res) => {
+  edits = req.body.edits;
+  console.log(edits);
+})
+
+// listening on port 3000
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+});
+
+connection.end();
